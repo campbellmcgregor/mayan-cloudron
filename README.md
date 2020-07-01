8936039c95bbd6fc2ad3a86b70695424ffbe066f
Euan <euanmc@pm.me>
2020-07-01 19:19:00 +0100

    upping memory
    container ran out of memory during tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-mayan-cloudron
+**MAYAN EDMS for Cloudron**
+
+This install can take upto 5 minutes as there are quite a lot of dependencies and database migrations to complete
--- start.sh
+++ start.sh
@@ -10,11 +10,14 @@ export MAYAN_DATABASE_USER=${CLOUDRON_POSTGRESQL_USERNAME}
 export MAYAN_DATABASE_PASSWORD=${CLOUDRON_POSTGRESQL_PASSWORD}
 export MAYAN_DATABASE_HOST=${CLOUDRON_POSTGRESQL_HOST}
 export MAYAN_DATABASE_PORT=${CLOUDRON_POSTGRESQL_PORT}
-export MAYAN_CELERY_RESULT_BACKEND="redis://:mayanredispassword@127.0.0.1:6379/1",
-export MAYAN_CELERY_BROKER_URL="redis://:mayanredispassword@127.0.0.1:6379/0",
+export MAYAN_CELERY_RESULT_BACKEND="redis://:${CLOUDRON_REDIS_PASSWORD}@${CLOUDRON_REDIS_HOST}:${CLOUDRON_REDIS_PORT}/1"
+export MAYAN_CELERY_BROKER_URL="redis://:${CLOUDRON_REDIS_PASSWORD}@${CLOUDRON_REDIS_HOST}:${CLOUDRON_REDIS_PORT}/0"
+
 export MAYAN_MEDIA_ROOT=/app/data/media
 
 /opt/mayan-edms/bin/mayan-edms.py initialsetup
+
+chown cloudron:cloudron /app/data -R
 
 #apt remove -y --purge libjpeg-dev libpq-dev libpng-dev libtiff-dev zlib1g-dev
 
